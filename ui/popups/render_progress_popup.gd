991e97330ec00f9b25b34d866eb16cc8f80395e0
Connor Lennox <clennox99@gmail.com>
2025-06-11 23:07:16 -0400

    better progress readout

--- ui/popups/render_progress_popup.gd
+++ ui/popups/render_progress_popup.gd
@@ -1,7 +1,8 @@
 class_name RenderProgressPopup
 extends Control
 
-@onready var label: Label = %Label
+@onready var title_label: Label = %TitleLabel
+@onready var progress_label: Label = %ProgressLabel
 @onready var progress_bar: ProgressBar = %ProgressBar
 
 func _ready():
@@ -10,7 +11,9 @@ func _ready():
 	SpritesheetRenderer.on_render_progress_update.connect(update_percentage)
 
 func popup(render_title: String):
-	label.text = "Rendering... (%s)" % render_title
+	title_label.text = "(%s)" % render_title
+	progress_label.text = "(Anim 0/?, Frame 0/?)"
+	
 	progress_bar.value = 0
 	visible = true
 
@@ -20,6 +23,7 @@ func unpopup():
 func update_percentage(current_frame: int, total_frames: int, current_animation: int, total_animations: int):
 	var percentage = _calculate_percentage(current_frame, total_frames, current_animation, total_animations)
 	progress_bar.value = percentage
+	progress_label.text = "(Anim %d/%d, Frame %d/%d)" % [current_animation + 1, total_animations, current_frame + 1, total_frames]
 
 func _calculate_percentage(current_frame: int, total_frames: int, current_animation: int, total_animations: int):
 	var progress_per_animation = 100.0 / total_animations
