235d2957bba59bf5e24ffd90ce7b1cabc501889b
Connor Lennox <clennox99@gmail.com>
2025-06-20 22:19:27 -0400

    backwards compatible flpx loader

--- util/flpx_handler.gd
+++ util/flpx_handler.gd
@@ -51,28 +51,55 @@ static func _flpx_to_dict(flpx: FlpxContents) -> Dictionary:
 static func _dict_to_flpx(dict: Dictionary) -> FlpxContents:
 	var flpx = FlpxContents.new()
 	
-	flpx.model_settings.model_path = dict["model"]["model_path"]
-	flpx.model_settings.available_animations = dict["model"]["available_animations"]
-	flpx.model_settings.selected_animations = dict["model"]["selected_animations"]
+	var model_dict = dict.get("model", {})
+	var camera_dict = dict.get("camera", {})
+	var render_dict = dict.get("render", {})
+	var export_dict = dict.get("export", {})
 	
-	flpx.camera_settings.model_rotation = dict["camera"]["model_rotation"]
-	flpx.camera_settings.camera_offset = str_to_var(dict["camera"]["camera_offset"])
-	flpx.camera_settings.camera_tilt = dict["camera"]["camera_tilt"]
-	flpx.camera_settings.ortho_camera = dict["camera"]["ortho_camera"]
-	flpx.camera_settings.ortho_camera_size = dict["camera"]["ortho_camera_size"]
-	flpx.camera_settings.perspective_camera_fov = dict["camera"]["perspective_camera_fov"]
-	flpx.camera_settings.turntable_steps = dict["camera"]["turntable_steps"]
+	if model_dict.has("model_path"):
+		flpx.model_settings.model_path = model_dict["model_path"]
+	if model_dict.has("available_animations"):
+		flpx.model_settings.available_animations = model_dict["available_animations"]
+	if model_dict.has("selected_animations"):
+		flpx.model_settings.selected_animations = model_dict["selected_animations"]
 	
-	flpx.render_settings.resolution = str_to_var(dict["render"]["resolution"])
-	flpx.render_settings.use_color_quantization = dict["render"]["use_color_quantization"]
-	flpx.render_settings.quantization_palette = (dict["render"]["quantization_palette"].map(func (c): return str_to_var(c)))
-	flpx.render_settings.render_fps = dict["render"]["render_fps"]
+	if camera_dict.has("model_rotation"):
+		flpx.camera_settings.model_rotation = camera_dict["model_rotation"]
+	if camera_dict.has("camera_offset"):
+		flpx.camera_settings.camera_offset = str_to_var(camera_dict["camera_offset"])
+	if camera_dict.has("camera_tilt"):
+		flpx.camera_settings.camera_tilt = camera_dict["camera_tilt"]
+	if camera_dict.has("ortho_camera"):
+		flpx.camera_settings.ortho_camera = camera_dict["ortho_camera"]
+	if camera_dict.has("ortho_camera_size"):
+		flpx.camera_settings.ortho_camera_size = camera_dict["ortho_camera_size"]
+	if camera_dict.has("perspective_camera_fov"):
+		flpx.camera_settings.perspective_camera_fov = camera_dict["perspective_camera_fov"]
+	if camera_dict.has("turntable_steps"):
+		flpx.camera_settings.turntable_steps = camera_dict["turntable_steps"]
 	
-	flpx.export_settings.export_path = dict["export"]["export_path"]
-	flpx.export_settings.spritesheet_columns = dict["export"]["spritesheet_columns"]
-	flpx.export_settings.export_normals = dict["export"]["export_normals"]
+	if render_dict.has("resolution"):
+		flpx.render_settings.resolution = str_to_var(render_dict["resolution"])
+	if render_dict.has("use_color_quantization"):
+		flpx.render_settings.use_color_quantization = render_dict["use_color_quantization"]
+	if render_dict.has("quantization_palette"):
+		flpx.render_settings.quantization_palette = (render_dict["quantization_palette"].map(func (c): return str_to_var(c)))
+	if render_dict.has("render_fps"):
+		flpx.render_settings.render_fps = render_dict["render_fps"]
+	
+	if export_dict.has("export_path"):
+		flpx.export_settings.export_path = export_dict["export_path"]
+	if export_dict.has("spritesheet_columns"):
+		flpx.export_settings.spritesheet_columns = export_dict["spritesheet_columns"]
+	if export_dict.has("export_normals"):
+		flpx.export_settings.export_normals = export_dict["export_normals"]
 	
 	return flpx
+
+
+static func _get_dict_value(dict: Dictionary, subdict: String, key: String, default: Variant = null):
+	var nested_dict = dict.get(subdict, {})
+	return nested_dict.get(key, default)
 
 
 static func save_session_flpx(path: String):
